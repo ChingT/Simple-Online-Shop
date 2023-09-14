8ce5177e964d9702a1cd9b69dab77dcc36bc9a82
ChingT <j60626j@gmail.com>
2023-09-14 17:57:51 +0200

    Fix login form

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -2,57 +2,65 @@ import { useState } from "react";
 import { api } from "../axios";
 import { useDispatch } from "react-redux";
 import { login, loadUser } from "../store/slices/user";
-import { useLocation, useNavigate } from "react-router-dom";
-import data from "../../user.json";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import userData from "../../user.json";
+import FormInput from "../components/FormInput";
 
 export default function Login() {
-  const [email, setEmail] = useState(data.email);
-  const [password, setPassword] = useState(data.password);
-  const [loginError, setLoginError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const location = useLocation();
-  console.log("Login location", location);
+  const [error, setError] = useState([]);
+  const [data, setData] = useState({
+    email: userData.email,
+    password: userData.password,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setData((prevProps) => ({ ...prevProps, [name]: value }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await api.post("/auth/token/", { email, password });
+      const res = await api.post("/auth/token/", data);
       console.log('api.post "/auth/token/"', res);
       dispatch(login(res.data.access));
       dispatch(loadUser(res.data.user));
       localStorage.setItem("accessToken", res.data.access);
-      setLoginError("");
+      setError("");
       const target = location.state?.from || "/account";
       console.log("location.state", location.state);
       console.log("navigate to", target);
       navigate(target);
     } catch (e) {
       console.log('api.post "/auth/token/" error', e);
-      setLoginError(e.response.data.detail);
+      setError(Object.values(e.response.data));
     }
   };
 
   return (
-    <form id="login" onSubmit={(e) => handleSubmit(e)}>
-      <input
-        type="email"
-        name="email"
-        placeholder="eMail"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Log in</button>
-      <p className="errorText">{loginError}</p>
-    </form>
+    <>
+      <h2>Log in</h2>
+
+      <form id="login" onSubmit={(e) => handleSubmit(e)}>
+        {FormInput("email", data.email, handleChange, "email")}
+        {FormInput("password", data.password, handleChange, "password")}
+
+        <button type="submit">Submit</button>
+        {error.map((err) => (
+          <p key={err} className="errorText">
+            {err}
+          </p>
+        ))}
+      </form>
+
+      <div className="hint">
+        <div>Do not have an account?</div>
+        <Link to="/register">Register here</Link>
+      </div>
+    </>
   );
 }
