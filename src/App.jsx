18801b58d0980b175dd9937d58c8bf1b33c13b22
ChingT <j60626j@gmail.com>
2023-09-15 23:40:49 +0200

    Add useReloadAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,11 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { motionAPI } from "./axios";
-import useFetch from "./hooks/useFetch";
+import useReloadAuth from "./hooks/useReloadAuth";
 import Router from "./routes";
-import { login, logout } from "./store/slices/user";
 
 function App() {
-  const accessToken = useSelector((state) => state.user.accessToken);
-  console.log("accessToken", accessToken);
-  const dispatch = useDispatch();
+  const { loading } = useReloadAuth();
 
-  const [config, setConfig] = useState(null);
-  const { resData, error } = useFetch(motionAPI, config);
-
-  const token = localStorage.getItem("accessToken");
-
-  useEffect(() => {
-    if (token) {
-      setConfig({
-        method: "post",
-        url: "/auth/token/verify/",
-        data: { token },
-      });
-    } else {
-      dispatch(logout());
-    }
-  }, [dispatch, token]);
-
-  useEffect(() => {
-    if (resData !== null) dispatch(login(token));
-  }, [dispatch, resData, token]);
-
-  useEffect(() => {
-    if (error) {
-      localStorage.removeItem("accessToken");
-      dispatch(logout());
-    }
-  }, [dispatch, error]);
-
-  if (accessToken || accessToken === null) return <Router />;
+  if (!loading) return <Router />;
   return <h2>Loading accessToken...</h2>;
 }
 
---
+++ src/hooks/useReloadAuth.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { motionAPI } from "../axios";
+import { login, logout } from "../store/slices/user";
+import useFetch from "./useFetch";
+
+export default function useReloadAuth() {
+  const dispatch = useDispatch();
+  const { sendRequest, resData, error, loading } = useFetch(motionAPI);
+  const token = localStorage.getItem("accessToken");
+  console.log("localStorage.accessToken", token);
+
+  useEffect(() => {
+    if (token) {
+      sendRequest({
+        method: "post",
+        url: "/auth/token/verify/",
+        data: { token },
+      });
+    } else {
+      dispatch(logout());
+    }
+  }, [dispatch, sendRequest, token]);
+
+  useEffect(() => {
+    if (resData !== null) {
+      dispatch(login(token));
+    }
+  }, [dispatch, resData, token]);
+
+  useEffect(() => {
+    if (error !== null) {
+      dispatch(logout());
+      localStorage.removeItem("accessToken");
+    }
+  }, [dispatch, error]);
+
+  return { loading };
+}
