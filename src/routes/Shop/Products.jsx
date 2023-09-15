484a30a867f7a9a198bc0c2266099a9b85ab904e
ChingT <j60626j@gmail.com>
2023-09-15 18:12:15 +0200

    Render Products

---
+++ src/routes/Shop/Products.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { DummyJsonAPI } from "../../axios";
+import Product from "../../components/Product";
+import useFetch from "../../hooks/useFetch";
+import "./index.css";
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  const [config, setConfig] = useState(null);
+  const { resData } = useFetch(DummyJsonAPI, config);
+
+  useEffect(() => {
+    setConfig({ method: "get", url: "/products", params: { limit: 12 } });
+  }, []);
+
+  useEffect(() => {
+    if (resData) setProducts(resData.products);
+  }, [resData]);
+
+  if (!products) return <h2>Loading posts...</h2>;
+
+  return (
+    <div id="products">
+      <h1>Crazy Shop</h1>
+      <ul>
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
-import Home from "./Home";
 import Layout from "./Layout";
 import ProtectedRoutes from "./ProtectedRoutes";
+import Products from "./Shop/Products";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Validate from "./auth/Validate";
@@ -13,7 +13,7 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Products />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/validate" element={<Validate />} />
