907537d63d2f53166b9d39d3f9db1278f468e59e
ChingT <j60626j@gmail.com>
2023-09-15 15:51:23 +0200

    USe useFetch in App, Account

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,30 +1,30 @@
-import { useEffect } from "react";
-import { api } from "../axios";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadUser } from "../store/slices/user";
+import useFetch from "../hooks/useFetch";
 
 export default function Account() {
   const token = useSelector((state) => state.user.accessToken);
   const details = useSelector((state) => state.user.details);
-  console.log("detals", details);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const config = { headers: { Authorization: `Bearer ${token}` } };
-        const res = await api.get("/users/me/", config);
-        console.log('pi.get "/users/me/"', res);
-        dispatch(loadUser(res.data));
-      } catch (e) {
-        console.log('pi.get "/users/me/" error', e);
-      }
-    };
+  const [config, setConfig] = useState(null);
+  const { resData } = useFetch(config);
 
-    fetchUser();
-  }, [dispatch, token]);
+  useEffect(() => {
+    setConfig({
+      method: "get",
+      url: "/users/me/",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  }, [token]);
+
+  useEffect(() => {
+    dispatch(loadUser(resData));
+  }, [dispatch, resData]);
 
   if (!details) return <h2>Loading user details...</h2>;
+
   return (
     <>
       User: {details.first_name} {details.last_name}
